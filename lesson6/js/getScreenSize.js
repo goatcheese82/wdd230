ec64c1c3cd5eb185a292cb899f68d2d2d029b5b0
goatcheese82 <euphoriawebdev@gmail.com>
2021-10-23 12:00:44 -0400

    6

--- lesson6/js/getScreenSize.js
+++ lesson6/js/getScreenSize.js
@@ -1,19 +1,21 @@
+const sizeIndicator = document.querySelector('#size-indicator');
+
 const getWidth = () => {
    let width = document.documentElement.clientWidth;
-   let image = document.querySelector('#hero-pic');
-   console.log(image)
    if (width > 1000) {
-      console.log('large')
       return "Large";
    }
    else if (width >= 600) {
-      console.log('medium')
       return "Medium"
       }
    else
-      console.log('small')
       return "Small";
 }
 
-window.addEventListener('resize', getWidth)
+const reportWidth = () => {
+   let size = getWidth();
+   sizeIndicator.innerHTML = size;
+}
+
+window.addEventListener('resize', reportWidth)
 
