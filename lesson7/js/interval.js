3c4f720a6fa4bc90db2e60c24d1adae8f0a04c9b
goatcheese82 <euphoriawebdev@gmail.com>
2021-11-06 15:50:08 -0400

    8

---
+++ lesson7/js/interval.js
@@ -0,0 +1,20 @@
+let today = Math.round(Date.now() / 8640000);
+const checkStorage = () => {
+   let node = document.getElementById('last-visit');
+   let lastVisit = (localStorage.getItem('lastVisit') != null) ? localStorage.getItem('lastVisit') : today;
+   if (lastVisit != today) {
+      let interval = today - lastVisit;
+      node.innerHTML = interval
+   }
+   else {
+      node.innerHTML = "Your last visit was today!";
+   }
+}
+
+const setLast = () => {
+   alert('you are closing the window')
+   localStorage.setItem('lastVisit', today)
+}
+
+checkStorage();
+window.onunload = () => setLast();
